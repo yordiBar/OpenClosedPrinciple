1a433700e08fee81131b2b357a4082c93243ec8f
Bart <bart.rybak@hotmail.com>
2022-06-18 23:56:37 +0100

    added Manager and Executive models and accounts classes

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,19 +11,18 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            List<PersonModel> applicants = new List<PersonModel>
+            List<IApplicantModel> applicants = new List<IApplicantModel>
             {
                 new PersonModel { FirstName = "Tim", LastName = "Corey" },
-                new PersonModel { FirstName = "Sue", LastName = "Storm"},
-                new PersonModel { FirstName = "Nancy", LastName = "Roman"}
+                new ManagerModel { FirstName = "Sue", LastName = "Storm"},
+                new ExecutiveModel { FirstName = "Nancy", LastName = "Roman"}
             };
 
             List<EmployeeModel> employees = new List<EmployeeModel>();
-            Accounts accountProcessor = new Accounts();
 
             foreach (var person in applicants)
             {
-                employees.Add(accountProcessor.Create(person));
+                employees.Add(person.AccountProcessor.Create(person));
             }
 
             foreach (var emp in employees)
